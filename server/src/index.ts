6a40d5da72d003df0e9eb6cce06e358aaee88806
darshaka1234 <darshakasrimal1234@gmail.com>
2023-12-18 22:49:11 +0530

    server setup done

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+app.use(express.json());
+
+const PORT = process.env.PORT || 6000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
